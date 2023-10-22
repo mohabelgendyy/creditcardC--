750a934e8e1463578599c4b206adb42df1b4a62a
mohabelgendyy <mohabelgendyy@github.com>
2023-10-22 18:45:06 +0300

    first commit

---
+++ creditcard.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+#include <iomanip>
+
+int getDigits(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
+int main()
+{
+    std::string cardNumber;
+    int result = 0;
+
+    std::cout << "Enter your credit card #: ";
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if (result % 10 == 0)
+    {
+        std::cout << " this is vaild";
+    }
+    else
+    {
+        std::cout << " this is not vaild";
+    }
+
+    return 0;
+}
+int getDigits(const int number)
+{
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits(const std::string cardNumber)
+{
+    int sum = 0;
+    for (int i = cardNumber.size() - 1; i >= 0; i -= 2)
+    {
+        sum += cardNumber[i] - '0';
+    }
+    return sum;
+}
+int sumEvenDigits(const std::string cardNumber)
+{
+    int sum = 0;
+    for (int i = cardNumber.size() - 2; i >= 0; i -= 2)
+    {
+        sum += getDigits((cardNumber[i] - '0') * 2);
+    }
+    return sum;
+}
